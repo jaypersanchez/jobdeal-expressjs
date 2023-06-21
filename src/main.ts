95bab4cd02d71d24251614d3c093b752f4ad73cb
ksbek <ksbek112@gmail.com>
2023-06-20 23:55:43 -0500

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .addTag('jobdeal')
     .addBearerAuth()
     .build();
+  app.enableCors();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
